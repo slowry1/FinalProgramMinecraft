494153388bd8c014b00ba7c20256844864b4e8cb
Scott Lowry <scotth@cpp.edu>
2015-11-01 16:05:37 -0800

    Almost done just need to make the square into a cube with different color walls

---
+++ src/finalprogramminecraft/FinalProgramMinecraft.java
@@ -0,0 +1,58 @@
+/*  Control the camera:
+*       W/S/A/D : Forward/Backward/Left/Right
+*       Space Bar: Move Up
+*       Left Shift: Move Down
+*       Mouse: Looks around
+*/
+package finalprogramminecraft;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.glu.GLU;
+
+public class FinalProgramMinecraft {
+    private FPCameraController fp = new FPCameraController(0f,0f,0f);
+    private DisplayMode displayMode;
+        
+    public static void main(String[] args) {
+    FinalProgramMinecraft basic = new FinalProgramMinecraft();
+    basic.start();
+    }
+    
+    public void start() {
+        try {
+            createWindow();
+            initGL();
+            fp.gameLoop();//render();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void createWindow() throws Exception{
+        Display.setFullscreen(false);
+        DisplayMode d[] = Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == 640 && d[i].getHeight() == 480 && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("Final Program Minecraft!");
+        Display.create();
+    }
+
+    private void initGL() {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        GLU.gluPerspective(100.0f, (float)displayMode.getWidth()/(float) displayMode.getHeight(), 0.1f, 300.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    }
+}
+
+
---
+++ src/finalprogramminecraft/Vector3Float.java
@@ -0,0 +1,12 @@
+// This class holds our camera’s position in 3D space.
+package finalprogramminecraft;
+
+public class Vector3Float {
+    public float x, y, z;
+    
+    public Vector3Float(int x, int y, int z){
+    this.x=x;
+    this.y=y;
+    this.z=z;
+    }
+}
