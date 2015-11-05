425f5c089bdc9e612c86a12fccbc68400385550b
Scott Lowry <scotth@cpp.edu>
2015-11-05 15:58:30 -0800

    Finished the first checkpoint. Moved the cube back to z = -1 as the starting point instead of z = 1 , so that the camera does not appear inside of it. Added the arrow keys to be able to move also.

--- src/finalprogramminecraft/FinalProgramMinecraft.java
+++ src/finalprogramminecraft/FinalProgramMinecraft.java
@@ -1,8 +1,31 @@
-/*  Control the camera:
-*       W/S/A/D : Forward/Backward/Left/Right
+/*******************************************************
+* file: FinalProgramMinecraft
+* authors: Scott Lowry, Daniel Lynch, Matt Capparelli
+* class: CS 445 - Computer Graphics - Fall Section 01
+*
+* assignment: Final Program
+* date last modified: 11/6/2015
+*
+* purpose: This program uses the LWJGL library to draw a window of 640x480
+* in the center of the screen. The program will create an original scene in 
+* Minecraft fashion.
+* 
+* Check Point 1 Requirements:
+* The program has a window created that is 640x480 and centered on the screen. 
+* It displays a cube (which is at least 2 in width) in 3D space with each face 
+* colored differently. The program allows the user to manipulate the camera with
+* the mouse to give a first person appearance and be able to navigate the 
+* environment using the input.Keyboard class with either the w,a,s,d keys or 
+* the arrow keys to move around as well as the space bar to move up and the 
+* left shift button to go down. The program allows the user to hit the escape 
+* key to quit the application.
+* 
+*    Control the camera:
+*       W/S/A/D And Up/Down/Left/Right (Arrow keys): Forward/Backward/Left/Right
 *       Space Bar: Move Up
 *       Left Shift: Move Down
 *       Mouse: Looks around
+*   The application was created using the guidelines from the slides from class.
 */
 package finalprogramminecraft;
 
@@ -52,6 +75,7 @@ public class FinalProgramMinecraft {
         GLU.gluPerspective(100.0f, (float)displayMode.getWidth()/(float) displayMode.getHeight(), 0.1f, 300.0f);
         glMatrixMode(GL_MODELVIEW);
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+        glEnable(GL_DEPTH_TEST);
     }
 }
 
