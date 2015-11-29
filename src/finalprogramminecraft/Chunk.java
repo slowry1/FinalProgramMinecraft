5d0cfa7a65b8cdba41431811fcebfd54ae8076c1
Scott Lowry <scotth@cpp.edu>
2015-11-28 16:17:11 -0800

    - Added stationary light to the scene.  need to figure out how to place it so half shadow is shown - Added texture check. Only place bedrock on the bottom, Grass on top (Needs to be changed to randomly select the items.)

--- src/finalprogramminecraft/Chunk.java
+++ src/finalprogramminecraft/Chunk.java
@@ -1,5 +1,18 @@
 package finalprogramminecraft;
 
+/*******************************************************
+* file: Chunk.java
+* authors: Scott Lowry, Daniel Lynch, Matt Capparelli
+* class: CS 445 - Computer Graphics - Fall Section 01
+*
+* assignment: Final Program
+* date last modified: 11/20/2015
+*
+* purpose: This is the Chunk object class, which is instantiated and represented 
+* in the window.  It handles the randomization of the terrain and applying the 
+* assigned textures to the appropriate blocks.
+*/
+
 import java.nio.FloatBuffer;
 import java.util.Random;
 import org.lwjgl.BufferUtils;
@@ -9,9 +22,14 @@ import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.util.ResourceLoader;
 
+//Chunk class stores block vertices in order to render them more efficiently
 public class Chunk {
-    static final int CHUNK_SIZE = 10;
+    
+    static final int CHUNK_SIZE = 30;
     static final int CUBE_LENGTH = 2;
+    static final float persistanceMin = 0.18f; 
+    static final float persistanceMax = 0.40f; 
+    
     private Block[][][] Blocks;
     private int VBOVertexHandle;
     private int VBOColorHandle;
@@ -19,11 +37,9 @@ public class Chunk {
     private int StartX, StartY, StartZ;
     private Random r;
     private Texture texture;
+    private static boolean topShelf = false;
     
-    private final int SIMPLEX_LARGEST_FEATURE = 5;
-    private final float SIMPLEX_PERSISTANCE = 0.5f;
-    private final int SIMPLEX_SEED = 7;
-    
+    //render the chunk
     public void render(){
         glPushMatrix();
         glBindBuffer(GL_ARRAY_BUFFER,VBOVertexHandle);
@@ -38,10 +54,20 @@ public class Chunk {
         glPopMatrix();
     }
     
+    //Primary method for generating terrain with simplex noise and drawing the chunk
     public void rebuildMesh(float startX, float startY, float startZ) {
         
-        SimplexNoise noise = new SimplexNoise(SIMPLEX_LARGEST_FEATURE, 
-                SIMPLEX_PERSISTANCE, SIMPLEX_SEED);
+        //Randomly generate a seed and persistence for the simplex noise
+        Random random = new Random();
+        
+        //persistance is between 0 and 1, 1=rocky, 0=smooth
+        float persistance = 0;
+        while (persistance < persistanceMin) {
+            persistance = (persistanceMax)*random.nextFloat();
+        }
+        int seed = (int)(50*random.nextFloat());
+        
+        SimplexNoise noise = new SimplexNoise(CHUNK_SIZE, persistance, seed);
         
         VBOColorHandle = glGenBuffers();
         VBOVertexHandle = glGenBuffers();
@@ -52,28 +78,40 @@ public class Chunk {
                 (CHUNK_SIZE* CHUNK_SIZE *CHUNK_SIZE) * 6 * 12);
         FloatBuffer VertexTextureData = BufferUtils.createFloatBuffer(
                 (CHUNK_SIZE*CHUNK_SIZE *CHUNK_SIZE)* 6 * 12);
+        
         for (float x = 0; x < CHUNK_SIZE; x += 1) {
             for (float z = 0; z < CHUNK_SIZE; z += 1) {
-                
-                //int i = (int)(startX + x*(CHUNK_SIZE/640));
-                //int j = (int)(startZ + z*(CHUNK_SIZE/480));
-                int height = (int)(startY + (int)(100*noise.getNoise((int)x,(int)z) * CUBE_LENGTH));
-                
-                
-                if (height>CHUNK_SIZE) {
-                    height = CHUNK_SIZE;
-                }
-                
-                for(float y = 0; y < height; y++){
+                for (float y = 0; y < CHUNK_SIZE; y++) {    
+                    
+                    //generate height from simplex noise
+                    int height = (int)(startY + Math.abs((int)(CHUNK_SIZE * noise.getNoise((int)x,(int)z))));
+                    System.out.println("This is height: "+height);
+                    System.out.println("This is y: "+y);
+                    if (y >= height) {
+                        break;
+                    }
+                    System.out.println("This is y2: "+y);
+                    
                     VertexPositionData.put( createCube(
                             (float)(startX + x* CUBE_LENGTH),
                             (float)(y*CUBE_LENGTH+(int)(CHUNK_SIZE*.8)),
                             (float)(startZ + z *CUBE_LENGTH)));
+    //                        0f,0f,0f));
                     
                     VertexColorData.put(createCubeVertexCol(getCubeColor(
                             Blocks[(int) x][(int) y][(int) z])));
+                    //Checks if it is bottom cube
+                    if(y == 0){
+                         VertexTextureData.put(createTexCube((float) 0, 
+                            (float)0,Blocks[(int)(x)][(int)(y)][(int) (z)]));
+                    }else if(y == height-1){ //If it is the top cube texture it with grass
+                        topShelf = true;
+                        VertexTextureData.put(createTexCube((float) 0, 
+                            (float)0,Blocks[(int)(x)][(int)(y)][(int) (z)]));
+                    }else{ //Not top or bottom cube                   
                     VertexTextureData.put(createTexCube((float) 0, 
-                            (float)0,Blocks[(int)(x)][(int) (y)][(int) (z)]));
+                            (float)0,Blocks[(int)(x)][(int)(y)][(int) (z)]));
+                    }
                 }
             }
         }
@@ -90,7 +128,8 @@ public class Chunk {
         glBufferData(GL_ARRAY_BUFFER, VertexTextureData,GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
-    
+
+    //Return an array of the colors on each sie of the cube
     private float[] createCubeVertexCol(float[] CubeColorArray) {
         float[] cubeColors = new float[CubeColorArray.length * 4 * 6];
         for (int i = 0; i < cubeColors.length; i++) {
@@ -100,6 +139,7 @@ public class Chunk {
         return cubeColors;
     }
 
+    //Create a cube at location x,y,z in the chunk by returning a set of vertexes
     public static float[] createCube(float x, float y, float z) {
         int offset = CUBE_LENGTH / 2;
         return new float[] {
@@ -135,11 +175,12 @@ public class Chunk {
             x + offset, y - offset, z };
     }
 
+    //Default cube color
     private float[] getCubeColor(Block block) {
         return new float[] { 1, 1, 1 };
     }
 
-
+    //Chunk constructor which assigns block types to all blocks and then calls rebuildMesh() to draw terrain
     public Chunk(int startX, int startY, int startZ) {
         try{texture = TextureLoader.getTexture("PNG", 
                 ResourceLoader.getResourceAsStream("terrain.png"));
@@ -149,20 +190,26 @@ public class Chunk {
         }
         r = new Random();
         Blocks = new Block[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE];
+        
         for (int x = 0; x < CHUNK_SIZE; x++) {
             for (int y = 0; y < CHUNK_SIZE; y++) {
                 for (int z = 0; z < CHUNK_SIZE; z++) {
-                    
+
                     //Block type is all psychadelic and shit because 
                     //rand is randomly generated each time the frame is updated...
                     //is it possible to set block type once only in the beginning?
                     //staticaly set block type?
-                    
-                    float rand = r.nextFloat();
-                    
-                    if(rand>0.830f){
-                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Grass);
-                    }else if(rand>0.664f){
+
+                 //   float rand = r.nextFloat();
+System.out.println("This is the y inside chunk: "+y);
+    if ( y == 0){
+        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Bedrock);
+    }else if(z == 29|| z == 0 || x == 29 || x == 0){              
+    //  if(rand>0.830f){
+                        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Dirt);
+    }else 
+        Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Water);
+        /*            }else if(rand>0.664f){
                         Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Sand);
                     }else if(rand>0.498f){
                         Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Dirt);
@@ -172,10 +219,11 @@ public class Chunk {
                         Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Stone);
                     }else{
                         Blocks[x][y][z] = new Block(Block.BlockType.BlockType_Bedrock);
-                    }
-                }
+                   }
+        */         }
             }
         }
+        
         VBOColorHandle = glGenBuffers();
         VBOVertexHandle = glGenBuffers();
         VBOTextureHandle = glGenBuffers();
@@ -185,9 +233,18 @@ public class Chunk {
         rebuildMesh(startX, startY, startZ);
     }
     
+    //Assigns textures from terrain.png to the sides of each cube
     public static float[] createTexCube(float x, float y, Block block) {
+        int tempBlockID;
         float offset = (1024f/16)/1024f;
-        switch (block.GetID()) {
+        // if the current block is the top block set texture to grass
+        if(topShelf){
+            tempBlockID = 0;
+            topShelf = false;
+        }else
+            tempBlockID = block.GetID();
+        
+        switch (tempBlockID) {
             case 0: //Grass block type
                 return new float[] {
                     // BOTTOM QUAD(DOWN=+Y)
@@ -319,67 +376,67 @@ public class Chunk {
             case 4: //stone block type
                 return new float[] {
                     // BOTTOM QUAD(DOWN=+Y)
-                    x + offset*5, y + offset*7,
+                    x + offset*5, y + offset*6,
+                    x + offset*6, y + offset*6,
                     x + offset*6, y + offset*7,
-                    x + offset*6, y + offset*8,
-                    x + offset*5, y + offset*8,
+                    x + offset*5, y + offset*7,
                     // TOP!
-                    x + offset*5, y + offset*7,
+                    x + offset*5, y + offset*6,
+                    x + offset*6, y + offset*6,
                     x + offset*6, y + offset*7,
-                    x + offset*6, y + offset*8,
-                    x + offset*5, y + offset*8,
+                    x + offset*5, y + offset*7,
                     // FRONT QUAD
-                    x + offset*5, y + offset*7,
+                    x + offset*5, y + offset*6,
+                    x + offset*6, y + offset*6,
                     x + offset*6, y + offset*7,
-                    x + offset*6, y + offset*8,
-                    x + offset*5, y + offset*8,
+                    x + offset*5, y + offset*7,
                     // BACK QUAD
-                    x + offset*5, y + offset*7,
+                    x + offset*5, y + offset*6,
+                    x + offset*6, y + offset*6,
                     x + offset*6, y + offset*7,
-                    x + offset*6, y + offset*8,
-                    x + offset*5, y + offset*8,
+                    x + offset*5, y + offset*7,
                     // LEFT QUAD
-                    x + offset*5, y + offset*7,
+                    x + offset*5, y + offset*6,
+                    x + offset*6, y + offset*6,
                     x + offset*6, y + offset*7,
-                    x + offset*6, y + offset*8,
-                    x + offset*5, y + offset*8,
+                    x + offset*5, y + offset*7,
                     // RIGHT QUAD
-                    x + offset*5, y + offset*7,
+                    x + offset*5, y + offset*6,
+                    x + offset*6, y + offset*6,
                     x + offset*6, y + offset*7,
-                    x + offset*6, y + offset*8,
-                    x + offset*5, y + offset*8};
+                    x + offset*5, y + offset*7};
             default: //bedrock block type
                 return new float[] {
                     // BOTTOM QUAD(DOWN=+Y)
-                    x + offset*0, y + offset*1,
                     x + offset*1, y + offset*1,
+                    x + offset*2, y + offset*1,
+                    x + offset*2, y + offset*2,
                     x + offset*1, y + offset*2,
-                    x + offset*0, y + offset*2,
                     // TOP!
-                    x + offset*0, y + offset*1,
                     x + offset*1, y + offset*1,
+                    x + offset*2, y + offset*1,
+                    x + offset*2, y + offset*2,
                     x + offset*1, y + offset*2,
-                    x + offset*0, y + offset*2,
                     // FRONT QUAD
-                    x + offset*0, y + offset*1,
                     x + offset*1, y + offset*1,
+                    x + offset*2, y + offset*1,
+                    x + offset*2, y + offset*2,
                     x + offset*1, y + offset*2,
-                    x + offset*0, y + offset*2,
                     // BACK QUAD
-                    x + offset*0, y + offset*1,
                     x + offset*1, y + offset*1,
+                    x + offset*2, y + offset*1,
+                    x + offset*2, y + offset*2,
                     x + offset*1, y + offset*2,
-                    x + offset*0, y + offset*2,
                     // LEFT QUAD
-                    x + offset*0, y + offset*1,
                     x + offset*1, y + offset*1,
+                    x + offset*2, y + offset*1,
+                    x + offset*2, y + offset*2,
                     x + offset*1, y + offset*2,
-                    x + offset*0, y + offset*2,
                     // RIGHT QUAD
-                    x + offset*0, y + offset*1,
                     x + offset*1, y + offset*1,
-                    x + offset*1, y + offset*2,
-                    x + offset*0, y + offset*2};
+                    x + offset*2, y + offset*1,
+                    x + offset*2, y + offset*2,
+                    x + offset*1, y + offset*2};
         }
     }
 }
--- src/finalprogramminecraft/FinalProgramMinecraft.java
+++ src/finalprogramminecraft/FinalProgramMinecraft.java
@@ -4,21 +4,22 @@
 * class: CS 445 - Computer Graphics - Fall Section 01
 *
 * assignment: Final Program
-* date last modified: 11/6/2015
+* date last modified: 11/20/2015
 *
 * purpose: This program uses the LWJGL library to draw a window of 640x480
 * in the center of the screen. The program will create an original scene in 
 * Minecraft fashion.
 * 
-* Check Point 1 Requirements:
+* Check Point 2 Requirements:
 * The program has a window created that is 640x480 and centered on the screen. 
-* It displays a cube (which is at least 2 in width) in 3D space with each face 
-* colored differently. The program allows the user to manipulate the camera with
-* the mouse to give a first person appearance and be able to navigate the 
-* environment using the input.Keyboard class with either the w,a,s,d keys or 
-* the arrow keys to move around as well as the space bar to move up and the 
-* left shift button to go down. The program allows the user to hit the escape 
-* key to quit the application.
+* It displays a 30x30 chunk of blocks with randomly generated block types: 
+* grass, sand, dirt, water, stone, or bedrock.  The chunk is also displayed 
+* with a randomly generated terrain . The program allows the user to manipulate 
+* the camera with the mouse to give a first person appearance and be able to 
+* navigate the environment using the input.Keyboard class with either the 
+* w,a,s,d keys or the arrow keys to move around as well as the space bar to 
+* move up and the left shift button to go down. The program allows the user to 
+* hit the escape key to quit the application.
 * 
 *    Control the camera:
 *       W/S/A/D And Up/Down/Left/Right (Arrow keys): Forward/Backward/Left/Right
@@ -34,16 +35,24 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.util.glu.GLU;
+import java.nio.FloatBuffer;
+import org.lwjgl.BufferUtils;
 
+//Main Class - contains the main method, creates frame
 public class FinalProgramMinecraft {
+    
     private FPCameraController fp; 
     private DisplayMode displayMode;
+    private FloatBuffer lightPosition;
+    private FloatBuffer whiteLight;
         
+    //main method
     public static void main(String[] args) {
-    FinalProgramMinecraft basic = new FinalProgramMinecraft();
-    basic.start();
+        FinalProgramMinecraft basic = new FinalProgramMinecraft();
+        basic.start();
     }
     
+    //create window and start game loop
     public void start() {
         try {
             createWindow();
@@ -55,6 +64,7 @@ public class FinalProgramMinecraft {
         }
     }
     
+    //Define window properties
     private void createWindow() throws Exception{
         Display.setFullscreen(false);
         DisplayMode d[] = Display.getAvailableDisplayModes();
@@ -69,6 +79,7 @@ public class FinalProgramMinecraft {
         Display.create();
     }
 
+    //Initialiaze GL properties
     private void initGL() {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glMatrixMode(GL_PROJECTION);
@@ -81,6 +92,20 @@ public class FinalProgramMinecraft {
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_TEXTURE_2D);
         glEnableClientState (GL_TEXTURE_COORD_ARRAY);
+        initLightArrays();
+        glLight(GL_LIGHT0, GL_POSITION, lightPosition); //sets our light’s position
+        glLight(GL_LIGHT0, GL_SPECULAR, whiteLight);//sets our specular light
+ //       glLight(GL_LIGHT0, GL_DIFFUSE, whiteLight);//sets our diffuse light
+        glLight(GL_LIGHT0, GL_AMBIENT, whiteLight);//sets our ambient light
+        glEnable(GL_LIGHTING);//enables our lighting
+        glEnable(GL_LIGHT0);//enables light0
+    }
+    
+    private void initLightArrays() {
+        lightPosition = BufferUtils.createFloatBuffer(4);
+        lightPosition.put(0.0f).put(0.0f).put(0.0f).put(1.0f).flip();
+        whiteLight = BufferUtils.createFloatBuffer(4);
+        whiteLight.put(1.0f).put(1.0f).put(1.0f).put(0.0f).flip();
     }
 }
 
